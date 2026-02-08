9738882412b43f581d77c67c8ca2d44f591ee4dc
egalo3 <egalo3@alumni.uv.es>
2026-02-08 20:56:32 +0100

    Perimetro Rectangulo

--- Perim.R
+++ Perim.R
@@ -4,6 +4,6 @@ PerimetroCuadrado <- function(L){
 LongitudCircunf <- function (r){
   return(2*3.14159*r)
 }
-PerimetroRectangulo <- function (b, h){
+PerimetroRectang <- function (b, h){
   return((b*h)*2)
 }
