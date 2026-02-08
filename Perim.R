c03e9deeaae081d2f7d630c472bb50e83b22387e
egalo3 <egalo3@alumni.uv.es>
2026-02-08 16:30:35 +0100

    Perimetro Rectangulo y cambio de pi

--- Perim.R
+++ Perim.R
@@ -2,5 +2,8 @@ PerimetroCuadrado <- function(L){
   return(4*L)
 }
 LongitudCircunf <- function (r){
-  return(2*pi*r)
+  return(2*3.14159*r)
+}
+PerimetroRectangulo <- function (b, h){
+  return((b*h)*2)
 }
